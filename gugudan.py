8ed850eeb2223422b0db6f14fba6a71739f84836
cdw8431 <cdw8431@gmail.com>
2022-05-29 14:23:08 +0900

    refactor: gugudan 로직 별도 함수로 분리

--- gugudan.py
+++ gugudan.py
@@ -1,4 +1,8 @@
-if __name__ == "__main__":
+def gugudan():
     for i in range(1, 10):
         for j in range(1, 10):
             print(f"{i} * {j} = {i * j}")
+
+
+if __name__ == "__main__":
+    gugudan()
