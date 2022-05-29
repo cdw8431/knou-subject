4e7110c64cfd6d0151d80d33a9ccb71f918b749a
cdw8431 <cdw8431@gmail.com>
2022-05-29 14:28:28 +0900

    feat: 종료 메세지 출력 기능 추가

--- gugudan.py
+++ gugudan.py
@@ -4,5 +4,12 @@ def gugudan():
             print(f"{i} * {j} = {i * j}")
 
 
+def print_end(end_message):
+    print(end_message)
+
+
 if __name__ == "__main__":
     gugudan()
+
+    end_message = "This is end."
+    print_end(end_message)
